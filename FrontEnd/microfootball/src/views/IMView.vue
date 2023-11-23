2c09311a4ffac31a50e5cb3be0817c64da96b6f6
litianlololo <102678203+litianlololo@users.noreply.github.com>
2023-11-23 18:17:28 +0800

    呃呃

--- FrontEnd/microfootball/src/views/IMView.vue
+++ FrontEnd/microfootball/src/views/IMView.vue
@@ -4,17 +4,13 @@
             <div :class="env?.isH5 ? 'conversation-h5' : 'conversation'"
                 v-show="!env?.isH5 || currentModel === 'conversation'">
                 <TUISearch class="search" />
-                <TUIConversation @current="handleCurrentConversation" />
+                <TUIConversation />
             </div>
             <div class="chat" v-show="!env?.isH5 || currentModel === 'message'">
                 <TUIChat>
-                    <h1>欢迎使用腾讯云即时通信IM</h1>
+                    
                 </TUIChat>
             </div>
-            <!-- TUICallKit 组件：通话 UI 组件主体 -->
-            <TUICallKit :class="!showCallMini ? 'callkit-drag-container' : 'callkit-drag-container-mini'"
-                :allowedMinimized="true" :allowedFullScreen="false" :beforeCalling="beforeCalling"
-                :afterCalling="afterCalling" :onMinimized="onMinimized" :onMessageSentByMe="onMessageSentByMe" />
         </div>
 
     </div>
