110a04a2b0d87e0d90756168c6237941fde6b868
litianlololo <102678203+litianlololo@users.noreply.github.com>
2023-11-23 14:12:58 +0800

    跨域

--- FrontEnd/micro-football/vue.config.js
+++ FrontEnd/micro-football/vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+        changeOrigin: true,
+        ws: true,                       //是否代理 websockets
+        secure: false,                   //是否https接口
+        pathRewrite: {                  //路径重置
+          '^/api': ''
+        }
+      }
+    }
+  },
 })
