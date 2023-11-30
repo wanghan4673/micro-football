d2422b2c973cab1428adcc8042075729fc4818fb
litianlololo <102678203+litianlololo@users.noreply.github.com>
2023-11-30 23:35:41 +0800

    我要当nacos糕手

--- BackEnd/User/src/main/java/com/football/user/userApplication.java
+++ BackEnd/User/src/main/java/com/football/user/userApplication.java
@@ -2,8 +2,10 @@ package com.football.user;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class userApplication {
     public static void main(String[] args) {
         SpringApplication.run(userApplication.class, args);
