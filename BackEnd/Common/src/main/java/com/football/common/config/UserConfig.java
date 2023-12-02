6cc848db0d0f2cd39d65c9e72e8752942c42f2fe
wang han <2226026498@qq.com>
2023-12-02 10:35:47 +0800

    解决网关共用配置类问题
    现在每个微服务只要在启动类中加入@Import(UserConfig.class)即可在需要获取用户id时使用UserContext.getUser()获取了

--- BackEnd/Common/src/main/java/com/football/common/config/UserConfig.java
+++ BackEnd/Common/src/main/java/com/football/common/config/UserConfig.java
@@ -1,14 +1,11 @@
 package com.football.common.config;
 
 import com.football.common.interceptors.UserInterceptor;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ConditionalOnClass(DispatcherServlet.class)
 public class UserConfig implements WebMvcConfigurer {
     // 配置类如果想生效 要被spring扫描包扫到才可以 但不同微服务的包不同 因此需要自动装配(在resource中)
     @Override
--- BackEnd/User/src/main/java/com/football/user/controller/ViewController.java
+++ BackEnd/User/src/main/java/com/football/user/controller/ViewController.java
@@ -1,7 +1,7 @@
 package com.football.user.controller;
 
+import com.football.common.utils.UserContext;
 import com.football.user.service.intf.UserService;
-import com.football.user.utils.AuthContext;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +15,7 @@ public class ViewController {
 
     @GetMapping("/user/score")
     public Integer showScore(){
-        Long userId = AuthContext.getUser();
+        Long userId = UserContext.getUser();
         log.info("userId is: "+userId);
         return 1;
     }
