df36c21dfdf0b74cbaee4486f8c0d11f3aad7fc8
litianlololo <102678203+litianlololo@users.noreply.github.com>
2023-12-06 20:36:03 +0800

    忘记改啥了，但是forum启动类补个enable

--- BackEnd/Forum/src/main/java/com/football/forum/ForumApplication.java
+++ BackEnd/Forum/src/main/java/com/football/forum/ForumApplication.java
@@ -3,13 +3,14 @@ package com.football.forum;
 import com.football.common.config.UserConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Import(UserConfig.class)
 public class ForumApplication {
     public static void main(String[] args) {
         SpringApplication.run(ForumApplication.class, args);
     }
-
 }
--- FrontEnd/microfootball/src/App.vue
+++ FrontEnd/microfootball/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="display: flex; justify-content: center;">
-    <TopNav v-if="topNav.topNavStatus" style="position: fixed; width: 100%; z-index: 99;"></TopNav>
+    <TopNav v-if="topNav.topNavStatus" style="position: fixed; width: 100%; z-index: 10;"></TopNav>
     <AdminTopMenu v-if="topNav.adminNavStatus" style="position: fixed; width: 100%;"></AdminTopMenu>
     <div :class="{ 'with-margin': topNav.topNavStatus || topNav.adminNavStatus }" style="width: 100%;">
       <router-view></router-view>
@@ -14,10 +14,12 @@ import AdminTopMenu from './components/AdminComponents/AdminTopMenu.vue'
 import { topNavStore } from './stores/topNav';
 
 const topNav = topNavStore()
+
 </script>
 
 <style>
 @import './assets/css/frame.scss';
+
 .with-margin {
   margin-top: 60px;
 }
