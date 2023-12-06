b2c45652efa22e75db62bd6fd15bc094ddd7298e
wang han <2226026498@qq.com>
2023-12-06 20:39:42 +0800

    增加News模块+网关配置

---
+++ BackEnd/News/src/main/java/com/football/news/NewsApplication.java
@@ -0,0 +1,13 @@
+package com.football.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NewsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NewsApplication.class, args);
+    }
+}
---
+++ BackEnd/News/src/main/java/com/football/news/controller/NewsController.java
@@ -0,0 +1,4 @@
+package com.football.news.controller;
+
+public class NewsController {
+}
