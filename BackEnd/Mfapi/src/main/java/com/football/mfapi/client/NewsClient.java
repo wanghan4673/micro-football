6b66f78bfe5659400c26f4d6f529ef6d83563fb6
Xie_yi <103160912+HeartFaker@users.noreply.github.com>
2023-12-23 14:36:01 +0800

    submit 1223 新闻微服务添加消息通信

--- BackEnd/Mfapi/src/main/java/com/football/mfapi/client/NewsClient.java
+++ BackEnd/Mfapi/src/main/java/com/football/mfapi/client/NewsClient.java
@@ -4,6 +4,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient()
+@FeignClient("news-service")
 public interface NewsClient {
 }
