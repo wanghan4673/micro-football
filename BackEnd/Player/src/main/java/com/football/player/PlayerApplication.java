1cb1c277603b79e2f0edc8efcabcba04dcd95bc5
MasouShinku <2389837103@qq.com>
2024-01-02 19:12:31 +0800

    引入mfapi依赖

--- BackEnd/Player/src/main/java/com/football/player/PlayerApplication.java
+++ BackEnd/Player/src/main/java/com/football/player/PlayerApplication.java
@@ -7,9 +7,11 @@ import com.football.player.service.impl.GameServiceImpl;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.football.mfapi")
 public class PlayerApplication {
     public static void main(String[] args) {
         SpringApplication.run(PlayerApplication.class, args);
