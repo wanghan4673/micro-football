7d59933329191cf26bf9af3d0e53b61ab41b301d
wang han <2226026498@qq.com>
2024-01-03 20:22:12 +0800

    修改UserController

--- BackEnd/User/src/main/java/com/football/user/controller/UserController.java
+++ BackEnd/User/src/main/java/com/football/user/controller/UserController.java
@@ -22,108 +22,6 @@ public class UserController {
     private final AnnouncementClient announcementClient;
     private final JwtUtils jwtUtils;
 
-    /* GET */
-    @GetMapping("/login-status")
-    public Result loginStatus() {
-        // 首页的导航栏判断是否登录 如果登录获取头像和用户名
-        Long userId = UserContext.getUser();
-        log.info("----------判断登录状态:{}----------", userId);
-        if (userId == null) {
-            return Result.success();
-        } else {
-            User user = userService.getNameAndAvatar(userId);
-            return Result.success(user);
-        }
-    }
-
-    @GetMapping("/user-info")
-    public Result getUserInfo() {
-        // 获取个人信息(包括name,signature,score,favorite_league,follow,fans)
-        Long userId = UserContext.getUser();
-        log.info("----------查看个人信息:{}----------", userId);
-        User user = userService.getUserInfo(userId);
-        return Result.success(user);
-    }
-
-    @GetMapping("/check-days")
-    public Result getCheckDays() {
-        // 获取签到天数
-        Long userId = UserContext.getUser();
-        log.info("----------获取签到天数:{}----------", userId);
-        List<String> checkDays = userService.getCheckDays(userId);
-        return Result.success(checkDays);
-    }
-
-    @GetMapping("/posts")
-    public Result getMyPosts() {
-        // 获取个人帖子
-        Long userId = UserContext.getUser();
-        log.info("----------获取我的帖子:{}----------", userId);
-        List<MyPost> myPosts = userService.getMyPosts(userId);
-        return Result.success(myPosts);
-    }
-
-    @GetMapping("/follow-list")
-    public Result getFollowList() {
-        // 获取关注列表
-        Long userId = UserContext.getUser();
-        log.info("----------获取关注列表:{}----------", userId);
-        List<User> myFollowers = userService.getMyFollowers(userId);
-        if (myFollowers != null) {
-            return Result.success(myFollowers);
-        } else {
-            return Result.error("获取关注列表失败");
-        }
-    }
-
-    @GetMapping("/fans-list")
-    public Result getFansList() {
-        // 获取粉丝列表
-        Long userId = UserContext.getUser();
-        log.info("----------获取粉丝列表:{}----------", userId);
-        List<User> myFans = userService.getMyFans(userId);
-        if (myFans != null) {
-            return Result.success(myFans);
-        } else {
-            return Result.error("获取粉丝列表失败");
-        }
-    }
-
-    @GetMapping("/notifications")
-    public Result getNotifications() {
-        // 获取通知
-        Long userId = UserContext.getUser();
-        log.info("----------获取通知:{}----------", userId);
-        return Result.success(announcementClient.getAnnouncementById(userId));
-    }
-
-    @GetMapping()
-    public List<AdminUsers> getAllUsers() {
-        // 获取所有用户
-        log.info("----------获取所有用户(为管理员准备)----------");
-        List<AdminUsers> adminUsers = userService.getAllUsers();
-        log.info(adminUsers.toString());
-        return adminUsers;
-    }
-
-    @GetMapping("/banned")
-    public List<AdminUsers> getBannedUsers() {
-        // 获取所有被举报用户
-        log.info("----------获取被举报用户(为管理员准备)----------");
-        List<AdminUsers> adminUsers = userService.getBannedUsers();
-        log.info(adminUsers.toString());
-        return adminUsers;
-    }
-
-    @GetMapping("/upcoming-games")
-    public Result getUpcomingGames() {
-        // 获取即将开始的关注比赛
-        Long userId = UserContext.getUser();
-        log.info("----------获取即将开始的比赛:{}----------", userId);
-//        String startTimeLimit = LocalDateTime.now().plusHours(24).toString();
-        List<GameSubscription> upcomingGames = userService.getGamesByUserId(userId);
-        return Result.success(upcomingGames);
-    }
 
     /* POST */
     @PostMapping("/login")
@@ -231,6 +129,109 @@ public class UserController {
         } else {
             return Result.error("关注失败");
         }
+    }
+
+    /* GET */
+    @GetMapping("/login-status")
+    public Result loginStatus() {
+        // 首页的导航栏判断是否登录 如果登录获取头像和用户名
+        Long userId = UserContext.getUser();
+        log.info("----------判断登录状态:{}----------", userId);
+        if (userId == null) {
+            return Result.success();
+        } else {
+            User user = userService.getNameAndAvatar(userId);
+            return Result.success(user);
+        }
+    }
+
+    @GetMapping("/user-info")
+    public Result getUserInfo() {
+        // 获取个人信息(包括name,signature,score,favorite_league,follow,fans)
+        Long userId = UserContext.getUser();
+        log.info("----------查看个人信息:{}----------", userId);
+        User user = userService.getUserInfo(userId);
+        return Result.success(user);
+    }
+
+    @GetMapping("/check-days")
+    public Result getCheckDays() {
+        // 获取签到天数
+        Long userId = UserContext.getUser();
+        log.info("----------获取签到天数:{}----------", userId);
+        List<String> checkDays = userService.getCheckDays(userId);
+        return Result.success(checkDays);
+    }
+
+    @GetMapping("/posts")
+    public Result getMyPosts() {
+        // 获取个人帖子
+        Long userId = UserContext.getUser();
+        log.info("----------获取我的帖子:{}----------", userId);
+        List<MyPost> myPosts = userService.getMyPosts(userId);
+        return Result.success(myPosts);
+    }
+
+    @GetMapping("/follow-list")
+    public Result getFollowList() {
+        // 获取关注列表
+        Long userId = UserContext.getUser();
+        log.info("----------获取关注列表:{}----------", userId);
+        List<User> myFollowers = userService.getMyFollowers(userId);
+        if (myFollowers != null) {
+            return Result.success(myFollowers);
+        } else {
+            return Result.error("获取关注列表失败");
+        }
+    }
+
+    @GetMapping("/fans-list")
+    public Result getFansList() {
+        // 获取粉丝列表
+        Long userId = UserContext.getUser();
+        log.info("----------获取粉丝列表:{}----------", userId);
+        List<User> myFans = userService.getMyFans(userId);
+        if (myFans != null) {
+            return Result.success(myFans);
+        } else {
+            return Result.error("获取粉丝列表失败");
+        }
+    }
+
+    @GetMapping("/notifications")
+    public Result getNotifications() {
+        // 获取通知
+        Long userId = UserContext.getUser();
+        log.info("----------获取通知:{}----------", userId);
+        return Result.success(announcementClient.getAnnouncementById(userId));
+    }
+
+    @GetMapping()
+    public List<AdminUsers> getAllUsers() {
+        // 获取所有用户
+        log.info("----------获取所有用户(为管理员准备)----------");
+        List<AdminUsers> adminUsers = userService.getAllUsers();
+        log.info(adminUsers.toString());
+        return adminUsers;
+    }
+
+    @GetMapping("/banned")
+    public List<AdminUsers> getBannedUsers() {
+        // 获取所有被举报用户
+        log.info("----------获取被举报用户(为管理员准备)----------");
+        List<AdminUsers> adminUsers = userService.getBannedUsers();
+        log.info(adminUsers.toString());
+        return adminUsers;
+    }
+
+    @GetMapping("/upcoming-games")
+    public Result getUpcomingGames() {
+        // 获取即将开始的关注比赛
+        Long userId = UserContext.getUser();
+        log.info("----------获取即将开始的比赛:{}----------", userId);
+//        String startTimeLimit = LocalDateTime.now().plusHours(24).toString();
+        List<GameSubscription> upcomingGames = userService.getGamesByUserId(userId);
+        return Result.success(upcomingGames);
     }
 
     /* DELETE */
